8ce38c94af4cb2d0f7563dae74b3d575677f69b8
arcslash <toarcslash@gmail.com>
2021-08-15 14:43:08 +0530

    addding hash derive

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
 )
 
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+}
+type BlockChain struct {
+	blocks []*Block
 }
 
 func (b *Block) DeriveHash() {
@@ -18,6 +22,14 @@ func (b *Block) DeriveHash() {
 
 }
 
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	//If this is gibberish to you look up
+	// pointer syntax in go
+	block.DeriveHash()
+	return block
+}
+
 func main() {
-	Println("Hello")
+	fmt.Println("Hello")
 }
