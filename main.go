553d7039717f6d8ca9e8d773e6e89ef6bdaad5bc
arcslash <isharaux@gmail.com>
2021-11-20 12:18:25 +0530

    finishing proof of work

--- main.go
+++ main.go
@@ -1,35 +1,28 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"github.com/arcslash/go-chain/blockchain"
+	"strconv"
 )
 
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-type BlockChain struct {
-	blocks []*Block
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	//If this is gibberish to you look up
-	// pointer syntax in go
-	block.DeriveHash()
-	return block
-}
-
 func main() {
-	fmt.Println("Hello")
+
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("first block after genesis")
+	chain.AddBlock("second block after genesis")
+	chain.AddBlock("third block after genesis")
+
+	for _, block := range chain.Blocks {
+
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("data: %s\n", block.Data)
+		fmt.Printf("hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProofOfWork(block)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+
 }
